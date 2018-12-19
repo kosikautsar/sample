f9801cb26a8ea1bad58a34f6a4f776c14303eed7
Yuda Fernanda <Yuda Fernanda>
2018-12-19 09:49:19 +0700

    perbaikan

--- hello.php
+++ hello.php
@@ -1,10 +1,10 @@
 <?php
 echo 'Hello World adalah Halo Dunia';
-echo '<br>'
+echo '<br>';
 echo 'This is my blog';
-echo '<br>'
+echo '<br>';
 echo 'All about Football';
-echo '<br>'
-echo '<br>'
+echo '<br>';
+echo '<br>';
 echo 'Today result match bologna 0 - 0 milan';
 ?>
